a93584a65197a841f8e97c6aa34ad09bae8c9d35
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-01-12 21:45:22 +0530

    Back to Top

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import BackTop from './Components/BackTop'
 import Navbar from './Components/Navbar'
 import Navbar2 from './Components/Navbar2'
 import CarDetail from './Pages/CarDetail'
 import Checkout from './Pages/Checkout'
+import Confirmed from './Pages/Confirmed'
 import Home from './Pages/Home'
 
 function App() {
+  
   return (
     <BrowserRouter>
+    <BackTop />
       <Routes>
         <Route path='/' element={<><Navbar /><Home /></>} />
         <Route path='/car/:id' element={<><Navbar2 /><CarDetail /></>} />
         <Route path='/checkout' element={<><Navbar2 /><Checkout /></>} />
+        <Route path='/confirmed' element={<><Navbar2 /><Confirmed /></>} />
       </Routes>
     </BrowserRouter>
   )
